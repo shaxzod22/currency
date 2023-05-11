21a4da5c9d5659ef6fdbda974a10fcf5c62f83c4
shaxzod22 <shaxzod01901@gmail.com>
2023-05-11 12:33:14 +0500

    currency site is finished

---
+++ js/sytles.js
@@ -0,0 +1,23 @@
+let elInput = document.querySelector('.input');
+let elBtn = document.querySelector('.btn');
+let elSelect = document.querySelector('.select')
+let result = document.querySelector('.result')
+
+const USDTOUZS = 11450.00
+const EUROTOUZS = 12514.85
+const RUBLTOUZS = 151.14
+
+elBtn.addEventListener('click' , function(event) {
+    event.preventDefault()
+    
+    let elValueSelect = elSelect.value
+    let elValueInput = elInput.value
+    
+    if(elValueSelect === 'usd'){
+        result.textContent = `~ ${Math.round(elValueInput * USDTOUZS)}`
+    } else if(elValueSelect === 'euro'){
+        result.textContent = `~ ${Math.round(elValueInput * EUROTOUZS)}`
+    } else{
+        result.textContent = `~ ${Math.round(elValueInput * RUBLTOUZS)}`
+    }
+})
